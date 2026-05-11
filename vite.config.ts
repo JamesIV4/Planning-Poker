5485f703e746834eb10edc437409d680e97e09f0
James Pound <james.pound@genesys.com>
2026-05-11 17:12:40 -0400

    Fix site URL issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/planning-poker/",
+  base: "/Planning-Poker/",
   test: {
     globals: true,
     environment: "jsdom",
